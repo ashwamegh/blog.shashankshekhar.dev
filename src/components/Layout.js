684909a2a1bbf92a7de86c03d7e40055b6a8f76c
shashank7200 <shashank7200@gmail.com>
2023-11-11 02:26:38 +0530

    updated logic to update blog theme

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -9,19 +9,29 @@ import '../styles/style.css'
 import '../styles/new-moon.css'
 
 export const Layout = ({ children }) => {
-  const [theme, setTheme] = useState('dark')
+  const [theme, setTheme] = useState('light')
 
   const onUpdateTheme = () => {
     const newTheme = theme === 'dark' ? 'light' : 'dark'
-    window.localStorage.setItem('theme', newTheme)
-    setTheme(newTheme)
+    if (typeof window !== 'undefined') {
+      window.localStorage.setItem('theme', newTheme)
+      setTheme(newTheme)
+    }
   }
 
   useEffect(() => {
-    const savedTheme = window.localStorage.getItem('theme')
-
-    if (savedTheme) {
-      setTheme(savedTheme)
+    if (typeof window !== 'undefined') {
+      const savedTheme = window.localStorage.getItem('theme')
+      if (savedTheme) {
+        setTheme(savedTheme)
+      } else {
+        try {
+          const isBrowserThemeDark = window.matchMedia(
+            '(prefers-color-scheme: dark)'
+          ).matches
+          isBrowserThemeDark && setTheme('dark')
+        } catch (e) {}
+      }
     }
   }, [])
 
